c92a699fd0b15e55bdc5059790a962516ecc598c
Chirasthi Herath <chirasthi192@gmail.com>
2024-06-09 13:52:32 +0530

    Redux

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Post from "./Post/Post";
 import useStyles from './styles';
 
 const Posts = () => {
-
+    const posts = useSelector((state)=> state.Posts);
     const classes = useStyles();
+
+    console.log(posts);
     return(
         <>
         <h1>POSTS</h1>
